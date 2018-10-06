122dc2e3637dc659320c7cd5234ef8fb69da86c4
Flixow <danielsiwek95@gmail.com>
2018-10-06 15:36:10 +0200

    Add basic components

--- components/Page.js
+++ components/Page.js
@@ -4,12 +4,16 @@ import { connect } from 'react-redux';
 import Clock from './Clock';
 import AddCount from './AddCount';
 
-const Page = ({ title, linkTo, lastUpdate, light }) => {
+import { register, login } from 'actions/auth';
+
+const Page = ({ title, linkTo, lastUpdate, light, register, login }) => {
   return (
     <div>
       <h1>{title}</h1>
       <Clock lastUpdate={lastUpdate} light={light} />
       <AddCount />
+      <button onClick={() => register({ email: 'test', password: 'test' })}>Register</button>
+      <button onClick={() => login({ email: 'test', password: 'test' })}>Login</button>
       <nav>
         <Link href={linkTo}><a>Navigate</a></Link>
       </nav>
@@ -17,4 +21,7 @@ const Page = ({ title, linkTo, lastUpdate, light }) => {
   );
 };
 
-export default connect(({ clock }) => clock)(Page);
+export default connect(({ clock }) => clock, {
+  register,
+  login,
+})(Page);
